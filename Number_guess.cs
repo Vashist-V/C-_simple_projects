94f70266f041cf95649bfdf46571650abe8a040f
Vashist V <118195547+Vashist-V@users.noreply.github.com>
2024-01-22 13:54:26 +0530

    Add files via upload
    Number guessing game program

---
+++ Number_guess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Number_guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random random = new Random();
+            int newRandom = random.Next(1, 101);
+
+            Console.WriteLine("A number between 1 and 10 will be generated...");
+            Console.WriteLine("If you guess the correct answer, You win the game");
+
+            while(!isCorrectGuess)
+            {
+                Console.WriteLine("Please enter your number: ");
+                int myNum = Convert.ToInt32(Console.ReadLine());
+
+                if (myNum > newRandom)
+                {
+                    Console.WriteLine("Your guess is too high");
+                }
+                else if(myNum < newRandom)
+                {
+                    Console.WriteLine("You guess is too low");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is correct");
+                    isCorrectGuess = true;
+                }
+                
+            }
+
+            Console.WriteLine("Congrats, you won the game!");
+            Console.ReadKey();
+
+        }
+    }
+}
