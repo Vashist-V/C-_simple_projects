6e54c2cc101f59e5252a227eb80b1f1958f3d6fa
Vashist V <118195547+Vashist-V@users.noreply.github.com>
2024-01-23 13:57:21 +0530

    Add files via upload

---
+++ Area_calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace areaCalculaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float result;
+            string answer;
+
+            Console.WriteLine("What shape would you like the find the area of?");
+            Console.WriteLine("Please enter 'r' for rectangle or enter any other key to find area of circle");
+
+            answer = Console.ReadLine();
+
+            if(answer == "r")
+            {
+                Console.WriteLine("Enter the height of rectangle: ");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the width of rectangle: ");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else
+            {
+                Console.WriteLine("Enter the radius of circle: ");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+            Console.WriteLine("The answer is : " +  result);
+            Console.ReadKey();
+        }
+        
+    }
+}
